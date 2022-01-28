e073d7785e2c9a2f18e8275bafdfb004741c3f39
raymond <mwaikasu21@gmail.com>
2022-01-28 21:51:51 +0300

    intial commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Github Actions Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Github Actions Demo"),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Card(
+              child: Text("Github Community"),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
